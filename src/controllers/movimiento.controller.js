d695a0f57a25a17ed450f071bd0f2509b0ed9aae
Juanpablo123456-Cyber <jpospinacast@gmail.com>
2021-05-15 16:57:15 -0500

    Crear movimiento modificado teniendo presente la diferencia de tipos sin suma ni resta

--- src/controllers/movimiento.controller.js
+++ src/controllers/movimiento.controller.js
@@ -1,45 +1,35 @@
 import Inventario from '../models/Inventario'
 import Movimiento from '../models/Movimiento'
 
-/*export const crearMovimiento = async (req, res) => {
-    const nuevoMovimientos = new Movimiento(req.body)
-    
-    try{
-        await nuevoMovimientos.save()
-        res.json({ ok: true })
-    }catch(ex){
-        console.log(ex.message)
-        res.status(400).json({ error: true })
-    }
-
-}*/
 export const crearMovimiento = async (req, res) => {
     try{
-        //const nuevoMovimientos = new Movimiento(req.body)
         const id_almacen=req.body.almacen
         const id_referencia=req.body.referencia
         let ver_inven= await Inventario.findOne({
             almacen: id_almacen,
             referencia: id_referencia
         })
-        try{
-        if(ver_inven.cantidad>=req.body.cantidad){
+        if(req.body.tipo == "adquisicion" || req.body.tipo == "dev_distribucion"){ //se compara adquicion o devolucion de distribucion teniendo en cuenta la cantidad del req
+            console.log("suma: " + req.body.tipo)
             const nuevoMovimiento = new Movimiento(req.body)
             await nuevoMovimiento.save() //agregar el nuevo movimiento
-            console.log(ver_inven.cantidad)
-            console.log(req.body.cantidad)
             res.json({ ok: true })
-        }}catch(ex){
-            console.log(ex.message)
-            res.status(400).json({ error: true,message: "La cantidad solicitada no puede ser mayor a la cantidad existente"})
-    }
-        /*else{
-            res.json({ok: false})
-            res.status(400).json({ error: true, message: "La cantidad solicitada no puede ser mayor a la cantidad existente"})
-        }*/ 
+        }else if(req.body.tipo == "dev_adquisicion" || req.body.tipo == "distribucion"){//se compara devolucion adquicion o distribucion teniendo en cuenta la cantidad del req
+            console.log("resta: " + req.body.tipo)
+            try{
+                if(ver_inven.cantidad>=req.body.cantidad){
+                    const nuevoMovimiento = new Movimiento(req.body)
+                    await nuevoMovimiento.save() //agregar el nuevo movimiento
+                    res.json({ ok: true })
+                }else{ex2}
+            }catch(ex2){
+                    console.log(ex2.message)
+                    res.status(400).json({ error: true,message: "La cantidad solicitada no puede ser mayor a la cantidad existente"})
+            }
+        }else{ex}
     }catch(ex){
             console.log(ex.message)
-            res.status(400).json({ error: true })
+            res.status(400).json({ error: true, message: "Tipo no valido" })
     }  
 
 }
