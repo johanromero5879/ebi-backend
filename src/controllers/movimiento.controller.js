9e9c5b449552f7ef00845c46dcd79ac1829cd915
Juanpablo123456-Cyber <jpospinacast@gmail.com>
2021-05-15 15:49:48 -0500

    Crear movimiento modicado con errores

--- src/controllers/movimiento.controller.js
+++ src/controllers/movimiento.controller.js
@@ -1,6 +1,7 @@
+import Inventario from '../models/Inventario'
 import Movimiento from '../models/Movimiento'
 
-export const crearMovimiento = async (req, res) => {
+/*export const crearMovimiento = async (req, res) => {
     const nuevoMovimientos = new Movimiento(req.body)
     
     try{
@@ -10,6 +11,36 @@ export const crearMovimiento = async (req, res) => {
         console.log(ex.message)
         res.status(400).json({ error: true })
     }
+
+}*/
+export const crearMovimiento = async (req, res) => {
+    try{
+        //const nuevoMovimientos = new Movimiento(req.body)
+        const id_almacen=req.body.almacen
+        const id_referencia=req.body.referencia
+        let ver_inven= await Inventario.findOne({
+            almacen: id_almacen,
+            referencia: id_referencia
+        })
+        try{
+        if(ver_inven.cantidad>=req.body.cantidad){
+            const nuevoMovimiento = new Movimiento(req.body)
+            await nuevoMovimiento.save() //agregar el nuevo movimiento
+            console.log(ver_inven.cantidad)
+            console.log(req.body.cantidad)
+            res.json({ ok: true })
+        }}catch(ex){
+            console.log(ex.message)
+            res.status(400).json({ error: true,message: "La cantidad solicitada no puede ser mayor a la cantidad existente"})
+    }
+        /*else{
+            res.json({ok: false})
+            res.status(400).json({ error: true, message: "La cantidad solicitada no puede ser mayor a la cantidad existente"})
+        }*/ 
+    }catch(ex){
+            console.log(ex.message)
+            res.status(400).json({ error: true })
+    }  
 
 }
 
