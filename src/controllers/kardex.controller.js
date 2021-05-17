66c73ca3f5f17cd6a638ac32ab87f5fee907cc78
Neyder Alfredo <neiderperilla032002@gmail.com>
2021-05-17 08:58:28 -0500

    modificacion kardex

--- src/controllers/kardex.controller.js
+++ src/controllers/kardex.controller.js
@@ -7,29 +7,12 @@ export const crearKardex = async (req, res) => {
                                             .sort({fecha: "desc"}).exec()
     try{
         if(ultimoKardex){
-            if((ultimoKardex.fecha.getMonth()+1 == 12 && nuevoKardex.fecha.getMonth()+1 == 1) && (ultimoKardex.fecha.getYear()+1 == nuevoKardex.fecha.getYear())){
-                
                 nuevoKardex.valorInicial =  ultimoKardex.valorFinal
                 nuevoKardex.cantidadInicial =  ultimoKardex.cantidadFinal
                 await nuevoKardex.save() //agregar el nuevo movimiento
         
                 res.json({ ok: true })
-                
-            }else if(ultimoKardex.fecha.getMonth()+1 == nuevoKardex.fecha.getMonth() && (ultimoKardex.fecha.getYear() == nuevoKardex.fecha.getYear())){
-                
-                nuevoKardex.valorInicial =  ultimoKardex.valorFinal
-                nuevoKardex.cantidadInicial =  ultimoKardex.cantidadFinal
-                
-                console.log(nuevoKardex.valorInicial+"        "+nuevoKardex.cantidadInicial)
-                await nuevoKardex.save() //agregar el nuevo movimiento
-        
-                res.json({ ok: true })
-            
-            }else{
-                    console.log(ex) //ex is not defined pero el funcionamiento es correcto
-                }
 
-            
         }else{
             nuevoKardex.valorInicial =  0
             nuevoKardex.cantidadInicial = 0
