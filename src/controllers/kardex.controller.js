ff2b57bcf842223e60729738f5e609ccad5ecdd1
Johan Romero <johan.romero5879@gmail.com>
2021-05-17 11:04:14 -0500

    Ajuste valores finales para crear kardex

--- src/controllers/kardex.controller.js
+++ src/controllers/kardex.controller.js
@@ -2,22 +2,22 @@ import Kardex from '../models/Kardex'
 import Movimiento from '../models/Movimiento'
 
 const calcularValoresFinales = (movimientos, kardex) => {
-    
+    kardex.valorFinal = 0
+    kardex.cantidadFinal = 0
 
     //Calcular valor y cantidad final del kardex
     for (const movimiento of movimientos) {
         const { cantidad, tipo, referencia } = movimiento
         const { precioCosto } = referencia
-
-        if(tipo in ["adquisicion", "dev_distribucion"]){
+        
+        if(tipo == "adquisicion" || tipo == "dev_distribucion"){
             kardex.cantidadFinal += cantidad
-            kardex.valorFinal += precioCosto * cantidad
-        }else if(tipo in ["distribucion", "dev_adquisicion"]){
+            kardexvalorFinal += precioCosto * cantidad
+        }else if(tipo == "distribucion" || tipo == "dev_adquisicion"){
             kardex.cantidadFinal -= cantidad
-            kardex.valorFinal -= (precioCosto * cantidad)
+            kardex.valorFinal -= precioCosto * cantidad
         }
     }
-
 }
 
 export const crearKardex = async (req, res) => {
@@ -35,13 +35,14 @@ export const crearKardex = async (req, res) => {
             nuevoKardex.cantidadInicial = 0
         }
 
-        const movimientos = await Movimiento.find(consultaMov, '_id cantidad tipo referencia') 
+        const movimientos = await Movimiento.find(consultaMov, { detalle: 0, almacen: 0 } ) 
                                         .populate('referencia', 'precioCosto')
                                         .sort({ fecha: "asc" }).exec()
 
         if(movimientos.length <= 0)
             throw "No se encontraron movimientos"
 
+        
         calcularValoresFinales(movimientos, nuevoKardex)    
         
         nuevoKardex.movimientos = movimientos.map((movimiento) => movimiento._id)
