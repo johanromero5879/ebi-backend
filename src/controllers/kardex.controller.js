a6ee0ad2783aa6b577914a699458c4a1e4c36321
Johan Romero <johan.romero5879@gmail.com>
2021-05-21 19:40:56 -0500

    Creacion y consuta de detalles de kardex

--- src/controllers/kardex.controller.js
+++ src/controllers/kardex.controller.js
@@ -20,6 +20,44 @@ const calcularValoresFinales = (movimientos, kardex) => {
     }
 }
 
+const detallarKardex = (kardex, movimientos) => {
+
+    kardex.movimientos.push({
+        fecha: kardex.fecha, 
+        tipo: 'Entrada',
+        detalle: 'Inventario inicial',
+        cantidad: kardex.cantidadInicial,
+        valorUnitario: '-',
+        valorTotal: kardex.valorFinal,
+        cantidadTotal: kardex.cantidadInicial,
+        saldoTotal: kardex.valorFinal
+    })
+
+    //Calcular valor y cantidad final del kardex
+    for (const movimiento of movimientos) {
+        const { cantidad, tipo, referencia, detalle, fecha } = movimiento
+        const { precioCosto } = referencia
+        if(tipo == "adquisicion" || tipo == "dev_distribucion"){
+            kardex.cantidadFinal += cantidad
+            kardex.valorFinal += precioCosto * cantidad
+        }else if(tipo == "distribucion" || tipo == "dev_adquisicion"){
+            kardex.cantidadFinal -= cantidad
+            kardex.valorFinal -= precioCosto * cantidad
+        }
+
+        kardex.movimientos.push({ 
+            fecha, 
+            tipo,
+            detalle,
+            cantidad,
+            valorUnitario: precioCosto,
+            valorTotal: precioCosto * cantidad,
+            cantidadTotal: kardex.cantidadFinal,
+            saldoTotal: kardex.valorFinal
+        })
+    }
+}
+
 export const crearKardex = async (req, res) => {
     const nuevoKardex = new Kardex(req.body)
     const ultimoKardex = await Kardex.findOne({ almacen: nuevoKardex.almacen })
@@ -40,7 +78,7 @@ export const crearKardex = async (req, res) => {
                                         .sort({ fecha: "asc" }).exec()
 
         if(movimientos.length <= 0)
-            throw "No se encontraron movimientos"
+            throw "No se encontraron movimientos recientes para generar el kardex"
 
         
         calcularValoresFinales(movimientos, nuevoKardex)    
@@ -55,4 +93,43 @@ export const crearKardex = async (req, res) => {
         res.status(400).json({ error: true, message: ex })
     }
 
+}
+
+export const obtenerKardexPorAlm = async (req, res) => {
+    const kardex = await Kardex.find({ almacen: req.params.id })
+    
+    res.json(kardex)
+}
+
+export const obtenerDetalleKardex = async (req, res) => {
+    try{
+        const kardex = await Kardex.findById(req.params.id)
+                                .populate({
+                                    path: 'movimientos',
+                                    populate: { path: 'referencia', select: '_id precioCosto' },
+                                    options: { sort: { fecha: 'asc' } }
+                                })
+        
+        if(!kardex)
+            throw { message: 'El kardex no existe' }
+        
+        console.log(kardex)
+
+        const detallesKardex = {
+            _id: kardex._id,
+            fecha: kardex.fecha,
+            valorInicial: kardex.valorInicial,
+            cantidadInicial: kardex.valorInicial,
+            valorFinal: kardex.valorInicial,
+            cantidadFinal: kardex.valorInicial,
+            movimientos: []
+        }
+
+        detallarKardex(detallesKardex, kardex.movimientos)
+        
+        res.json(detallesKardex)
+    }catch(ex){
+        res.status(400).json({ error: true, message: ex.message })
+    }
+    
 }
--- src/routes/kardex.routes.js
+++ src/routes/kardex.routes.js
@@ -1,18 +1,23 @@
 import { Router } from 'express'
 import {
-    crearKardex
+    crearKardex,
+    obtenerKardexPorAlm,
+    obtenerDetalleKardex
 } from '../controllers/kardex.controller'
 
 const router = Router()
 
-/*router.route('/referencia/:id')
-    .get(obtenerMovimientoReferencia)
+// router.route('/referencia/:id')
+//     .get(obtenerMovimientoReferencia)
 
 router.route('/almacen/:id')
-    .get(obtenerMovimientoAlmacen)*/
+    .get(obtenerKardexPorAlm)
 
 router.route('/')
     .post(crearKardex)
+
+router.route('/:id')
+    .get(obtenerDetalleKardex)
 
 
 export default router
